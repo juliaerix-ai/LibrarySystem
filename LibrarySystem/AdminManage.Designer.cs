03701e7043a4bd9b8ea9a83ce8564d7e36d40dde
CompLab512-PC1 <CompLab512-PC1@COMPLAB503-PC23>
2025-11-15 17:30:17 +0800

    Update (Manage)

--- LibrarySystem/AdminManage.Designer.cs
+++ LibrarySystem/AdminManage.Designer.cs
@@ -42,26 +42,21 @@
             button2 = new Button();
             button3 = new Button();
             panel1 = new Panel();
-            label1 = new Label();
             button10 = new Button();
             label2 = new Label();
             button11 = new Button();
-            pictureBox3 = new PictureBox();
-            pictureBox4 = new PictureBox();
-            pictureBox5 = new PictureBox();
-            pictureBox6 = new PictureBox();
-            pictureBox7 = new PictureBox();
-            pictureBox8 = new PictureBox();
+            button4 = new Button();
+            dataGridView1 = new DataGridView();
+            BookLog = new DataGridViewTextBoxColumn();
+            BookTitle = new DataGridViewTextBoxColumn();
+            Author = new DataGridViewTextBoxColumn();
+            StudNo = new DataGridViewTextBoxColumn();
+            Status = new DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             panel2.SuspendLayout();
             panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // pictureBox2
@@ -210,43 +205,26 @@
             // panel1
             // 
             panel1.BackColor = Color.WhiteSmoke;
-            panel1.Controls.Add(pictureBox8);
-            panel1.Controls.Add(pictureBox7);
-            panel1.Controls.Add(pictureBox6);
-            panel1.Controls.Add(pictureBox5);
-            panel1.Controls.Add(pictureBox4);
-            panel1.Controls.Add(pictureBox3);
+            panel1.Controls.Add(dataGridView1);
+            panel1.Controls.Add(button4);
             panel1.Controls.Add(button11);
             panel1.Controls.Add(label2);
             panel1.Controls.Add(button10);
-            panel1.Controls.Add(label1);
             panel1.Location = new Point(158, 71);
             panel1.Name = "panel1";
             panel1.Size = new Size(614, 350);
             panel1.TabIndex = 57;
             // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.BackColor = Color.Transparent;
-            label1.Font = new Font("Elephant", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.ForeColor = SystemColors.ActiveCaptionText;
-            label1.Location = new Point(14, 19);
-            label1.Name = "label1";
-            label1.Size = new Size(179, 31);
-            label1.TabIndex = 58;
-            label1.Text = "ADD BOOK";
-            // 
             // button10
             // 
             button10.FlatStyle = FlatStyle.Flat;
-            button10.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button10.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button10.ImageAlign = ContentAlignment.TopCenter;
-            button10.Location = new Point(48, 75);
+            button10.Location = new Point(452, 293);
             button10.Name = "button10";
-            button10.Size = new Size(99, 83);
+            button10.Size = new Size(145, 45);
             button10.TabIndex = 59;
-            button10.Text = "+";
+            button10.Text = "ADD BOOK";
             button10.UseVisualStyleBackColor = true;
             // 
             // label2
@@ -257,93 +235,67 @@
             label2.ForeColor = SystemColors.ActiveCaptionText;
             label2.Location = new Point(17, 184);
             label2.Name = "label2";
-            label2.Size = new Size(223, 27);
+            label2.Size = new Size(0, 27);
             label2.TabIndex = 60;
-            label2.Text = "ARCHIVE BOOK";
             // 
             // button11
             // 
             button11.FlatStyle = FlatStyle.Flat;
-            button11.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button11.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button11.ImageAlign = ContentAlignment.TopCenter;
-            button11.Location = new Point(48, 244);
+            button11.Location = new Point(301, 293);
             button11.Name = "button11";
-            button11.Size = new Size(99, 83);
+            button11.Size = new Size(145, 45);
             button11.TabIndex = 61;
-            button11.Text = "-";
+            button11.Text = "ARCHIVE BOOK";
             button11.UseVisualStyleBackColor = true;
             // 
-            // pictureBox3
+            // button4
             // 
-            pictureBox3.BackColor = Color.Gainsboro;
-            pictureBox3.BackgroundImageLayout = ImageLayout.Stretch;
-            pictureBox3.BorderStyle = BorderStyle.Fixed3D;
-            pictureBox3.Location = new Point(246, 40);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(101, 136);
-            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox3.TabIndex = 62;
-            pictureBox3.TabStop = false;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.ImageAlign = ContentAlignment.TopCenter;
+            button4.Location = new Point(17, 293);
+            button4.Name = "button4";
+            button4.Size = new Size(145, 45);
+            button4.TabIndex = 67;
+            button4.Text = "Book Detail";
+            button4.UseVisualStyleBackColor = true;
             // 
-            // pictureBox4
+            // dataGridView1
             // 
-            pictureBox4.BackColor = Color.Gainsboro;
-            pictureBox4.BackgroundImageLayout = ImageLayout.Stretch;
-            pictureBox4.BorderStyle = BorderStyle.Fixed3D;
-            pictureBox4.Location = new Point(246, 202);
-            pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(101, 136);
-            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox4.TabIndex = 63;
-            pictureBox4.TabStop = false;
+            dataGridView1.BackgroundColor = SystemColors.Highlight;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { BookLog, BookTitle, Author, StudNo, Status });
+            dataGridView1.Location = new Point(16, 20);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(583, 244);
+            dataGridView1.TabIndex = 68;
             // 
-            // pictureBox5
+            // BookLog
             // 
-            pictureBox5.BackColor = Color.Gainsboro;
-            pictureBox5.BackgroundImageLayout = ImageLayout.Stretch;
-            pictureBox5.BorderStyle = BorderStyle.Fixed3D;
-            pictureBox5.Location = new Point(379, 40);
-            pictureBox5.Name = "pictureBox5";
-            pictureBox5.Size = new Size(101, 136);
-            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox5.TabIndex = 64;
-            pictureBox5.TabStop = false;
+            BookLog.HeaderText = "BOOK LOG ID";
+            BookLog.Name = "BookLog";
             // 
-            // pictureBox6
+            // BookTitle
             // 
-            pictureBox6.BackColor = Color.Gainsboro;
-            pictureBox6.BackgroundImageLayout = ImageLayout.Stretch;
-            pictureBox6.BorderStyle = BorderStyle.Fixed3D;
-            pictureBox6.Location = new Point(379, 201);
-            pictureBox6.Name = "pictureBox6";
-            pictureBox6.Size = new Size(101, 136);
-            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox6.TabIndex = 65;
-            pictureBox6.TabStop = false;
+            BookTitle.HeaderText = "BOOK TITLE";
+            BookTitle.Name = "BookTitle";
             // 
-            // pictureBox7
+            // Author
             // 
-            pictureBox7.BackColor = Color.Gainsboro;
-            pictureBox7.BackgroundImageLayout = ImageLayout.Stretch;
-            pictureBox7.BorderStyle = BorderStyle.Fixed3D;
-            pictureBox7.Location = new Point(510, 40);
-            pictureBox7.Name = "pictureBox7";
-            pictureBox7.Size = new Size(101, 136);
-            pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox7.TabIndex = 66;
-            pictureBox7.TabStop = false;
+            Author.HeaderText = "AUTHOR";
+            Author.Name = "Author";
             // 
-            // pictureBox8
+            // StudNo
             // 
-            pictureBox8.BackColor = Color.Gainsboro;
-            pictureBox8.BackgroundImageLayout = ImageLayout.Stretch;
-            pictureBox8.BorderStyle = BorderStyle.Fixed3D;
-            pictureBox8.Location = new Point(510, 201);
-            pictureBox8.Name = "pictureBox8";
-            pictureBox8.Size = new Size(101, 136);
-            pictureBox8.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox8.TabIndex = 67;
-            pictureBox8.TabStop = false;
+            StudNo.HeaderText = "Book Type";
+            StudNo.Name = "StudNo";
+            // 
+            // Status
+            // 
+            Status.HeaderText = "STATUS";
+            Status.Name = "Status";
             // 
             // AdminManage
             // 
@@ -366,12 +318,7 @@
             panel2.PerformLayout();
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -392,14 +339,14 @@
         private Button button3;
         private Panel panel1;
         private Button button10;
-        private Label label1;
         private Button button11;
         private Label label2;
-        private PictureBox pictureBox4;
-        private PictureBox pictureBox3;
-        private PictureBox pictureBox8;
-        private PictureBox pictureBox7;
-        private PictureBox pictureBox6;
-        private PictureBox pictureBox5;
+        private Button button4;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn BookLog;
+        private DataGridViewTextBoxColumn BookTitle;
+        private DataGridViewTextBoxColumn Author;
+        private DataGridViewTextBoxColumn StudNo;
+        private DataGridViewTextBoxColumn Status;
     }
 }
